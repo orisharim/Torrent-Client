1f81eb1ef7fd49fd4c364991d2b0e6eb1b18aab6
ori sharim <ori.sharim@gmail.com>
2026-04-29 19:01:05 +0300

    added interested and choked behavior

--- peers/peer_manager.py
+++ peers/peer_manager.py
@@ -23,7 +23,6 @@ class PeerManager:
         self.downloaded_pieces: set[int] = set()
         self.requested_pieces: set[int] = set()
         
-        
         self.paused = False
 
     async def set_peers(self, peers_info: list[tuple[str, int]]) -> None:
@@ -31,62 +30,57 @@ class PeerManager:
         for ip, port in peers_info:
             self.peers.append(PeerConnection(ip, port, self.info_hash, self.peer_id))
 
-    async def add_new_peers(self, new_peers_info: list[tuple[str, int]]) -> None:
+    async def add_peers(self, new_peers_info: list[tuple[str, int]]) -> None:
         for ip, port in new_peers_info:
             self.peers.append(PeerConnection(ip, port, self.info_hash, self.peer_id))
     
-    
     async def connect_to_unconnected_peers(self) -> None:        
-        async def _connect_to_peer(peer: PeerConnection) -> None:
-            if peer.bitfield is not None: #connect to peer only if we haven't already connected to it and got its bitfield
-                return
-            try:
-                await peer.connect()
-                await peer.send_handshake()
-                await peer.read_bitfield()
-                  
-            except Exception as e:
-                print(f"Failed to connect to peer {peer.host}:{peer.port}: {e}")
         async with TaskGroup() as tg:
             for peer in self.peers:
-                tg.create_task(_connect_to_peer(peer))
+                if peer.bitfield is not None: #connect to peer only if we haven't already connected to it and got its bitfield
+                    continue
+                tg.create_task(self._connect_to_peer(peer))
         
     async def connect_to_all_peers(self) -> None:
         await self.close_all()
         
-        async def _connect_to_peer(peer: PeerConnection) -> None:
-            if peer.bitfield is not None: #connect to peer only if we haven't already connected to it and got its bitfield
-                return
-            try:
+        async with TaskGroup() as tg:
+            for peer in self.peers:
+                tg.create_task(self._connect_to_peer(peer))
+    
+    async def _connect_to_peer(self, peer: PeerConnection) -> None:
+        if peer.bitfield is not None: #connect to peer only if we haven't already connected to it and got its bitfield
+            return
+        try:
+            async with asyncio.timeout(10):    
                 await peer.connect()
                 await peer.send_handshake()
                 await peer.read_bitfield()
-                  
-            except Exception as e:
-                print(f"Failed to connect to peer {peer.host}:{peer.port}: {e}")
+
+        except Exception as e:
+            print(f"Failed to connect to peer {peer.host}:{peer.port}: {e}")
+    
             
-        async with TaskGroup() as tg:
-            for peer in self.peers:
-                tg.create_task(_connect_to_peer(peer))
-        
     async def close_all(self) -> None:
         for peer in self.peers:
             await peer.close()
             
     async def pause_downloads(self) -> None:
         self.paused = True
+        for peer in self.peers:
+            if peer.interested:
+                await peer.send_not_interested()
+
+    async def resume_downloads(self) -> None:
+        self.paused = False
     
     async def download_pieces(self) -> None:
         if self.paused:
             return
-
-        download_tasks = []
-        for _ in range(self.PIECES_AT_ONCE):
-            task = asyncio.create_task(self._download_piece())
-            download_tasks.append(task)
-
-        await asyncio.gather(*download_tasks)
-       
+        async with TaskGroup() as tg:
+            for _ in range(self.PIECES_AT_ONCE):
+                tg.create_task(self._download_piece())
+            
     async def _download_piece(self) -> Optional[Piece]:
         next_piece = await self._get_next_piece_index()
         if next_piece is None:
@@ -94,13 +88,31 @@ class PeerManager:
 
         piece_index, selected_peer = next_piece
         self.requested_pieces.add(piece_index)
-        piece = await self._request_piece(selected_peer, piece_index)
-        if piece is None:
+        try:
+            if self.paused:
+                return None
+
+            if not selected_peer.interested:
+                await selected_peer.send_interested()
+
+            if selected_peer.choked:
+                return None
+
+            blocks: list[tuple[int, int, bytes]] = []
+            for begin in range(0, self.piece_length, PeerConnection.DEFAULT_BLOCK_LENGTH):
+                if self.paused:
+                    await selected_peer.send_not_interested()
+                    return None
+                block_length = min(PeerConnection.DEFAULT_BLOCK_LENGTH, self.piece_length - begin)
+                await selected_peer.request_block(piece_index, begin, block_length)
+                block_index, block_begin, block = await selected_peer.read_block()
+                blocks.append((block_index, block_begin, block))
+
+            piece = Piece(piece_index, blocks)
+            self.downloaded_pieces.add(piece_index)
+            return piece # TODO: store pieces to storage
+        finally:
             self.requested_pieces.discard(piece_index)
-            return None
-        self.downloaded_pieces.add(piece_index)
-        self.requested_pieces.discard(piece_index)
-        return piece # TODO: store pieces to storage
 
     async def _get_next_piece_index(self) -> Optional[tuple[int, PeerConnection]]:
         
@@ -139,7 +151,7 @@ class PeerManager:
         chosen_peer = random.choice(chosen_piece[1])
         return chosen_piece[0], chosen_peer
 
-    # takes a bitfield and a piece idx and returns whether the bitfield indicates that the piece is there
+    # takes a bitfield and a piece idx and returns whether the bitfield indicates that the piece is there(doesnt have to be our bitfield, can be a peer's bitfield)
     def _has_piece(self, bitfield: bytes, piece_index: int) -> bool:
         byte_index = piece_index // 8
         if byte_index < 0 or byte_index >= len(bitfield):
@@ -150,18 +162,4 @@ class PeerManager:
         return (bitfield[byte_index] & mask) != 0
 
 
-    async def _request_piece(self, peer: PeerConnection, piece_index: int) -> Optional[Piece]:
-        if peer.choked:
-            return None
-
-        # request blocks of the piece
-        piece_length = self.piece_length
-        blocks: list[tuple[int, int, bytes]] = []
-        for begin in range(0, piece_length, PeerConnection.DEFAULT_BLOCK_LENGTH):
-            block_length = min(PeerConnection.DEFAULT_BLOCK_LENGTH, piece_length - begin)
-            await peer.request_block(piece_index, begin, block_length)
-            block_index, block_begin, block = await peer.read_block()
-            blocks.append((block_index, block_begin, block))
-
-        return Piece(piece_index, blocks)
     
